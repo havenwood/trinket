c1ea3a0219ab1c0e2dc240c98cbfe810919d9362
Shannon Skipper <shannonskipper@gmail.com>
2019-11-28 07:39:29 -0800

    Extract method refactor

--- lib/trinket_server.rb
+++ lib/trinket_server.rb
@@ -4,28 +4,38 @@ require 'trinket_services_pb'
 
 module Trinket
   class Server < Trinket::Gems::Service
+    VERSION_PARTS = %i[major minor teeny patch].freeze
+
     def gem(request, _)
-      matching_gems = ::Gem::Specification.latest_specs.select do |spec|
+      specs = ::Gem::Specification.latest_specs.select do |spec|
         spec.name.match? Regexp.new request.name
       end
 
-      gems = matching_gems.map do |spec|
-        segments = %i[major minor teeny patch].zip(spec.version.segments).select { |_, v| v }.to_h
+      Trinket::Response.new gems: gems(specs)
+    end
+
+    private
+
+    def gems(specs)
+      specs.map do |spec|
+        segments = VERSION_PARTS.zip(spec.version.segments).to_h.compact
         version = Trinket::Version.new string: spec.version.to_s, **segments
 
-        Trinket::Gem.new(
-          author_count: spec.authors.size,
-          authors: spec.authors,
-          description: spec.description.to_s,
-          epoch_time: spec.date.to_i,
-          homepage: spec.homepage.to_s,
-          name: spec.name,
-          summary: spec.summary.to_s,
-          version: version
-        )
+        trinket_gem(spec, version)
       end
+    end
 
-      Trinket::Response.new gems: gems
+    def trinket_gem(spec, version)
+      Trinket::Gem.new(
+        author_count: spec.authors.size,
+        authors: spec.authors,
+        description: spec.description.to_s,
+        epoch_time: spec.date.to_i,
+        homepage: spec.homepage.to_s,
+        name: spec.name,
+        summary: spec.summary.to_s,
+        version: version
+      )
     end
   end
 end
