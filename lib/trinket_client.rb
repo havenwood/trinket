aeee47923a6de59a839b45b4a745d54fc55f6851
Shannon Skipper <shannonskipper@gmail.com>
2019-11-28 07:34:19 -0800

    Appease the linter

--- lib/trinket_client.rb
+++ lib/trinket_client.rb
@@ -1,14 +1,18 @@
+# frozen_string_literal: true
+
 require 'singleton'
 require 'trinket_services_pb'
 
-class Trinket::Client
-  include Singleton
+module Trinket
+  class Client
+    include Singleton
 
-  def initialize
-    @channel = Trinket::Gems::Stub.new 'localhost:50051', :this_channel_is_insecure
-  end
+    def initialize
+      @channel = Trinket::Gems::Stub.new 'localhost:50051', :this_channel_is_insecure
+    end
 
-  def gem name:
-    @channel.gem Trinket::Request.new name: name
+    def gem(name:)
+      @channel.gem Trinket::Request.new name: name
+    end
   end
 end
--- lib/trinket_server.rb
+++ lib/trinket_server.rb
@@ -1,25 +1,31 @@
+# frozen_string_literal: true
+
 require 'trinket_services_pb'
 
-class Trinket::Server < Trinket::Gems::Service
-  def gem request, _
-    matching_gems = Gem::Specification.latest_specs.select do |spec|
-      spec.name.match? Regexp.new request.name
-    end.map do |spec|
-      segments = %i[major minor teeny patch].zip(spec.version.segments).select { |_, v| v }.to_h
-      version = Trinket::Version.new string: spec.version.to_s, **segments
+module Trinket
+  class Server < Trinket::Gems::Service
+    def gem(request, _)
+      matching_gems = Gem::Specification.latest_specs.select do |spec|
+        spec.name.match? Regexp.new request.name
+      end
 
-      Trinket::Gem.new(
-        author_count: spec.authors.size,
-        authors: spec.authors,
-        description: spec.description.to_s,
-        epoch_time: spec.date.to_i,
-        homepage: spec.homepage.to_s,
-        name: spec.name,
-        summary: spec.summary.to_s,
-        version: version
-      )
+      gems = matching_gems.map do |spec|
+        segments = %i[major minor teeny patch].zip(spec.version.segments).select { |_, v| v }.to_h
+        version = Trinket::Version.new string: spec.version.to_s, **segments
+
+        Trinket::Gem.new(
+          author_count: spec.authors.size,
+          authors: spec.authors,
+          description: spec.description.to_s,
+          epoch_time: spec.date.to_i,
+          homepage: spec.homepage.to_s,
+          name: spec.name,
+          summary: spec.summary.to_s,
+          version: version
+        )
+      end
+
+      Trinket::Response.new gems: gems
     end
-
-    Trinket::Response.new gems: matching_gems
   end
 end
